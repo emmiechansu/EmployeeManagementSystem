113234c2fdb598070cb7ef52024920d7bd7dfa38
MJ Pepito <mj.pepito.01@gmail.com>
2026-04-21 11:57:40 +0800

    Initial commit

---
+++ EmployeeManagementSystem/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmployeeManagementSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool run = true;
+
+            while (run)
+            {
+                "test if it's working"
+            }
+        }
+    }
+}
