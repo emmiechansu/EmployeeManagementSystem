15aa6b883a2a452573a195dd2f28b1c1c284bbe4
MJ Pepito <mj.pepito.01@gmail.com>
2026-04-22 15:52:58 +0800

    Stop tracking .vs folder

--- EmployeeManagementDataService/obj/Debug/net8.0/EmployeeManagementDataService.AssemblyInfo.cs
+++ EmployeeManagementDataService/obj/Debug/net8.0/EmployeeManagementDataService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementDataService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32865d3779df8f2dadbb0197f04657802094475b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+600c7a9f63016188454484485a83ccde6e4fb830")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementDataService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementDataService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EmployeeManagementSystem/obj/Debug/net8.0/EmployeeManagementSystem.AssemblyInfo.cs
+++ EmployeeManagementSystem/obj/Debug/net8.0/EmployeeManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32865d3779df8f2dadbb0197f04657802094475b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+600c7a9f63016188454484485a83ccde6e4fb830")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
